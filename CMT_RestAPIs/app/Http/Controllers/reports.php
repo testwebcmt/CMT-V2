838bab5cdc939e5b8f64a08113d2e4b454c8ce05
testwebcmt <77197815+testwebcmt@users.noreply.github.com>
2021-02-12 18:44:13 +0000

    report_completed

--- CMT_RestAPIs/app/Http/Controllers/reports.php
+++ CMT_RestAPIs/app/Http/Controllers/reports.php
@@ -161,4 +161,290 @@ class reports extends BaseController
                
                 
     }
+
+    public function goalreport(Request $request)
+    {
+
+     
+         if ($request->json()->has('category'))
+                {  
+                    $catname= $request->json()->get('category');  
+                  
+                    if ($request->json()->has('programName')) 
+                        {
+
+                            $programname = $request->json()->get('programName');
+
+                            if ($request->json()->has('zipCode')) 
+                                {
+                                      $zipcode = $request->json()->get('zipCode');        
+                  
+                                      $result = DB::table('tb_init_user_details')
+                                              ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                                              ->select('tb_init_user_details.userId','tb_init_user_details.firstName','tb_init_user_details.lastName','tb_init_user_details.email','tb_init_user_details.phoneCell','tb_init_user_details.zipCode','tb_init_user_goals.user_goal_category_name','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_RatingBefore','tb_init_user_goals.user_goal_program_RatingAfter') 
+                                              ->where('tb_init_user_goals.user_goal_category_name', $catname)
+                                              ->where('tb_init_user_goals.user_goal_program_name',$programname)
+                                              ->where('tb_init_user_details.zipCode',$zipcode)
+                                              ->groupBy('tb_init_user_details.userId')
+                                              ->get(); 
+  
+                                  $search['report'] = $result;
+  
+                                  return response($search,200);
+
+                            }
+
+                          else
+                            {
+
+                              $result = DB::table('tb_init_user_details')
+                              ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                              ->select('tb_init_user_details.userId','tb_init_user_details.firstName','tb_init_user_details.lastName','tb_init_user_details.email','tb_init_user_details.phoneCell','tb_init_user_details.zipCode','tb_init_user_goals.user_goal_category_name','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_RatingBefore','tb_init_user_goals.user_goal_program_RatingAfter') 
+                              ->where('tb_init_user_goals.user_goal_category_name', $catname)
+                              ->where('tb_init_user_goals.user_goal_program_name',$programname)
+                              ->groupBy('tb_init_user_details.userId')
+                              ->get(); 
+
+                               $search['report'] = $result;
+
+                              return response($search,200);
+
+
+                            }
+                        }
+
+                        else if ($request->json()->has('zipCode')) 
+                        {
+                              $zipcode = $request->json()->get('zipCode');        
+          
+                              $result = DB::table('tb_init_user_details')
+                              ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                              ->select('tb_init_user_details.userId','tb_init_user_details.firstName','tb_init_user_details.lastName','tb_init_user_details.email','tb_init_user_details.phoneCell','tb_init_user_details.zipCode','tb_init_user_goals.user_goal_category_name','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_RatingBefore','tb_init_user_goals.user_goal_program_RatingAfter') 
+                              ->where('tb_init_user_goals.user_goal_category_name', $catname)
+                              ->where('tb_init_user_details.zipCode',$zipcode)
+                              ->groupBy('tb_init_user_details.userId')
+                              ->get();  
+
+                          $search['report'] = $result;
+
+                          return response($search,200);
+
+                         }
+                            $catname= $request->json()->get('category');        
+                  
+                            $result = DB::table('tb_init_user_details')
+                            ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                            ->select('tb_init_user_details.userId','tb_init_user_details.firstName','tb_init_user_details.lastName','tb_init_user_details.email','tb_init_user_details.phoneCell','tb_init_user_details.zipCode','tb_init_user_goals.user_goal_category_name','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_RatingBefore','tb_init_user_goals.user_goal_program_RatingAfter') 
+                            ->where('tb_init_user_goals.user_goal_category_name', $catname)
+                            ->groupBy('tb_init_user_details.userId')
+                            ->get(); 
+
+                            $search['report'] = $result;
+
+                            return response($search,200);
+
+                }
+         else if ($request->json()->has('programName'))
+                {
+
+                  $programname = $request->json()->get('programName');
+
+                  if($request->json()->has('zipCode'))
+
+                      {
+
+                        $zipcode = $request->json()->get('zipCode');                          
+
+                        $result = DB::table('tb_init_user_details')
+                                  ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                                  ->select('tb_init_user_details.userId','tb_init_user_details.firstName','tb_init_user_details.lastName','tb_init_user_details.email','tb_init_user_details.phoneCell','tb_init_user_details.zipCode','tb_init_user_goals.user_goal_category_name','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_RatingBefore','tb_init_user_goals.user_goal_program_RatingAfter') 
+                                  ->where('tb_init_user_goals.user_goal_program_name',$programname)
+                                  ->where('tb_init_user_details.zipCode',$zipcode)
+                                  ->groupBy('tb_init_user_details.userId')
+                                  ->get(); 
+
+                         $search['report'] = $result;
+
+                         return response($search,200);
+
+                      }
+
+                        
+                      $result = DB::table('tb_init_user_details')
+                      ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                      ->select('tb_init_user_details.userId','tb_init_user_details.firstName','tb_init_user_details.lastName','tb_init_user_details.email','tb_init_user_details.phoneCell','tb_init_user_details.zipCode','tb_init_user_goals.user_goal_category_name','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_RatingBefore','tb_init_user_goals.user_goal_program_RatingAfter') 
+                      ->where('tb_init_user_goals.user_goal_program_name',$programname)
+                      ->groupBy('tb_init_user_details.userId')
+                      ->get(); 
+
+                          $search['report'] = $result;
+
+                          return response($search,200);
+
+                }
+
+         else if ($request->json()->has('zipCode'))
+                {
+                           
+                                $zipcode = $request->json()->get('zipCode');
+
+                                $result = DB::table('tb_init_user_details')
+                                ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                                ->select('tb_init_user_details.userId','tb_init_user_details.firstName','tb_init_user_details.lastName','tb_init_user_details.email','tb_init_user_details.phoneCell','tb_init_user_details.zipCode','tb_init_user_goals.user_goal_category_name','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_RatingBefore','tb_init_user_goals.user_goal_program_RatingAfter') 
+                                ->where('tb_init_user_details.zipCode',$zipcode)
+                                ->groupBy('tb_init_user_details.userId')
+                                ->get();  
+
+                                $search['report'] = $result;
+
+                                return response($search,200);
+
+                }
+               
+                
+    }
+
+    public function notesreport(Request $request)
+    {
+
+     
+         if ($request->json()->has('category'))
+                {  
+                    $catname= $request->json()->get('category');  
+                  
+                    if ($request->json()->has('programName')) 
+                        {
+
+                            $programname = $request->json()->get('programName');
+
+                            if ($request->json()->has('zipCode')) 
+                                {
+                                      $zipcode = $request->json()->get('zipCode');        
+                  
+                                      $result = DB::table('tb_init_user_details')
+                                              ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                                              ->select('tb_init_user_details.userId','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_status','tb_init_user_goals.user_goal_program_participantcomments','tb_init_user_goals.user_goal_program_additionalcomments','tb_init_user_details.notes') 
+                                              ->where('tb_init_user_goals.user_goal_category_name', $catname)
+                                              ->where('tb_init_user_goals.user_goal_program_name',$programname)
+                                              ->where('tb_init_user_details.zipCode',$zipcode)
+                                              ->groupBy('tb_init_user_details.userId')
+                                              ->get(); 
+  
+                                  $search['report'] = $result;
+  
+                                  return response($search,200);
+
+                            }
+
+                          else
+                            {
+
+                              $result = DB::table('tb_init_user_details')
+                              ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                              ->select('tb_init_user_details.userId','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_status','tb_init_user_goals.user_goal_program_participantcomments','tb_init_user_goals.user_goal_program_additionalcomments','tb_init_user_details.notes') 
+                              ->where('tb_init_user_goals.user_goal_category_name', $catname)
+                              ->where('tb_init_user_goals.user_goal_program_name',$programname)
+                              ->groupBy('tb_init_user_details.userId')
+                              ->get(); 
+
+                               $search['report'] = $result;
+
+                              return response($search,200);
+
+
+                            }
+                        }
+
+                        else if ($request->json()->has('zipCode')) 
+                        {
+                              $zipcode = $request->json()->get('zipCode');        
+          
+                              $result = DB::table('tb_init_user_details')
+                              ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                              ->select('tb_init_user_details.userId','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_status','tb_init_user_goals.user_goal_program_participantcomments','tb_init_user_goals.user_goal_program_additionalcomments','tb_init_user_details.notes') 
+                              ->where('tb_init_user_goals.user_goal_category_name', $catname)
+                              ->where('tb_init_user_details.zipCode',$zipcode)
+                              ->groupBy('tb_init_user_details.userId')
+                              ->get();  
+
+                          $search['report'] = $result;
+
+                          return response($search,200);
+
+                         }
+                            $catname= $request->json()->get('category');        
+                  
+                            $result = DB::table('tb_init_user_details')
+                            ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                            ->select('tb_init_user_details.userId','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_status','tb_init_user_goals.user_goal_program_participantcomments','tb_init_user_goals.user_goal_program_additionalcomments','tb_init_user_details.notes') 
+                            ->where('tb_init_user_goals.user_goal_category_name', $catname)
+                            ->groupBy('tb_init_user_details.userId')
+                            ->get(); 
+
+                            $search['report'] = $result;
+
+                            return response($search,200);
+
+                }
+         else if ($request->json()->has('programName'))
+                {
+
+                  $programname = $request->json()->get('programName');
+
+                  if($request->json()->has('zipCode'))
+
+                      {
+
+                        $zipcode = $request->json()->get('zipCode');                          
+
+                        $result = DB::table('tb_init_user_details')
+                                  ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                                  ->select('tb_init_user_details.userId','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_status','tb_init_user_goals.user_goal_program_participantcomments','tb_init_user_goals.user_goal_program_additionalcomments','tb_init_user_details.notes') 
+                                  ->where('tb_init_user_goals.user_goal_program_name',$programname)
+                                  ->where('tb_init_user_details.zipCode',$zipcode)
+                                  ->groupBy('tb_init_user_details.userId')
+                                  ->get(); 
+
+                         $search['report'] = $result;
+
+                         return response($search,200);
+
+                      }
+
+                        
+                      $result = DB::table('tb_init_user_details')
+                      ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                      ->select('tb_init_user_details.userId','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_status','tb_init_user_goals.user_goal_program_participantcomments','tb_init_user_goals.user_goal_program_additionalcomments','tb_init_user_details.notes') 
+                      ->where('tb_init_user_goals.user_goal_program_name',$programname)
+                      ->groupBy('tb_init_user_details.userId')
+                      ->get(); 
+
+                          $search['report'] = $result;
+
+                          return response($search,200);
+
+                }
+
+         else if ($request->json()->has('zipCode'))
+                {
+                           
+                                $zipcode = $request->json()->get('zipCode');
+
+                                $result = DB::table('tb_init_user_details')
+                                ->join('tb_init_user_goals`','tb_init_user_details.userId','=','tb_init_user_program_details.userId') 
+                                ->select('tb_init_user_details.userId','tb_init_user_goals.user_goal_program_name','tb_init_user_goals.user_goal_program_status','tb_init_user_goals.user_goal_program_participantcomments','tb_init_user_goals.user_goal_program_additionalcomments','tb_init_user_details.notes') 
+                                ->where('tb_init_user_details.zipCode',$zipcode)
+                                ->groupBy('tb_init_user_details.userId')
+                                ->get();  
+
+                                $search['report'] = $result;
+
+                                return response($search,200);
+
+                }
+               
+                
+    }  
+
+
 }
